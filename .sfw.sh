4a540ed696af7bc55733144fb72c5dea24ab241e
C <chaichunyang@outlook.com>
2020-05-26 11:59:37 +0800

    sfw and nsfw switch key binding

---
+++ .sfw.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# random a wallpaper and lockscreen image from ~/.wallpaper
+
+# wallpaper
+feh -rz --bg-fill --no-fehbg ~/.wallpaper/
+
+# lockscreen
+~/.i3lock-gen-image.sh ~/.wallpaper/
+
