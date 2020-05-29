ca164ef2cad5b8dd8f1d40da2736f0c94cd741ee
C <chaichunyang@outlook.com>
2020-05-29 13:36:38 +0800

    some update

--- .start.sh
+++ .start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 feh -rz --no-fehbg --bg-fill ~/.wallpaper/
 
-echo ${WALLPAPER} > ~/.wallpaper.txt
+echo "~/.wallpaper/" > ~/.wallpaper.txt
 
 if [ -f "/usr/bin/betterlockscreen" ]; then
 betterlockscreen -b 2 -u $1
